4ece55236cca663994b79f4d169df21e11089b8c
0x6d61736b6564 <116027312+0x6d61736b6564@users.noreply.github.com>
2022-12-02 11:29:48 +0100

    Update raid_bot.py

--- raid_bot.py
+++ raid_bot.py
@@ -1,6 +1,4 @@
 from discord.ext import commands
-from discord.errors import Forbidden
-import discord.ext
 from discord.ext.commands import has_permissions, MissingPermissions
 from os import system,name
 import discord
@@ -35,14 +33,31 @@ async def on_ready():
     print(f"Original prefix : ! ")
     print(f"Commands: do \033[4m\033[1m#help\033[0m\033[0m in a channel")
 
+
+
 @bot.command(name="clear", pass_context=True)
 @commands.has_permissions(manage_messages=True)
-async def clear(ctx):
-  while 1:
-    await ctx.channel.purge(limit=1000)
+async def clear(ctx, amount: int):
+    await ctx.channel.purge(limit=amount+1)
+    await ctx.message.delete()
 
 
+@bot.command()
+async def help(ctx):
+        embed=discord.Embed(title="Help menu", url="https://discord.gg/zsWMEHx2Ss", description="_**Default Prefix: ! **_ \n  1 **clear** - clear des messages ex: !clear 2  \n 2 **kick** - kick un membre du serveur \n 3 _**nuke**_ - nuke le serveur et ajouter un nouveau salon 'nigger' \n 4 **special** - nuke le serveur et ajouter un nouveau salon 'nimporte' = ex: special ligma+balls \n 5 **spam** - spam messages = example: spam huh_? \n 6 **channel** - spam salons : 'Vive masked' \n 7 **spamchannel** - spam salons = ex: spamchannel omg 10 \n 8 **vocal** - créer un salon vocal ex: vocal hey 1 \n 9 **auto** - nuke & fait toutes les commandes raid \n **Spaces are __+__**", color=0xFF5733)
+        await ctx.send(embed=embed)
 
+@client.event
+async def on_member_join(member):
+      general_channel: discord.TextChannel = client.get_channel(1034849491972542474)
+      general_channel.send(content=f"Bienvenue sur le serveur {member.display_name} !")
+
+@client.event
+async def on_message(message):
+    if message.content.lower() == "quoi":
+        await message.channel.send("Feur")
+    if message.content.lower()== "méchant":
+        await message.delete()
 
 @bot.command(name="nuke", pass_context=True)
 @commands.has_permissions(ban_members=True)
@@ -132,10 +147,8 @@ async def auto(ctx):
 @bot.command(name="kick", pass_context=True)
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason=None):
-    if await member.kick(reason=reason):
-      await ctx.send(f'{member} à été kick du serveur.')
-    else:
-      await ctx.send(f'{member} ne peux pas être kick du serveur.')
+    await client.kick(member)
+    await ctx.send(f'User {member} has been kick')
 
 token = "Your token"
 bot.run(token)
